544f58683ad157c18eb95ef2d0f2829f683cb9d4
k2k-com <nizamovevgenij@gmail.com>
2022-04-23 00:08:16 +0300

    added poduct-slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,23 @@
 $(function () {
 
+    $('.product-slider__inner').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        dots: true,
+        arrows: false,
+       focusOnSelect: true,
+        cssEase: 'linear'
+    });
+
+
+
+
+
+
+
+
     var mixer = mixitup('.products__inner-box');
 
-});  
+});
+
+
