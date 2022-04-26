01a07e0dc8c307be92686fbccb4af33a5a696342
k2k-com <nizamovevgenij@gmail.com>
2022-04-27 00:45:13 +0300

    new-items page

--- app/js/main.js
+++ app/js/main.js
@@ -8,6 +8,28 @@ $(function () {
         focusOnSelect: true,
     });
 
+    $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        min: 0,
+        max: 1000,
+        from: 0,
+        to: 600,
+        prefix: "$"
+    });
+
+    $('.icon-windows').on('click', function(){
+     $('.products__item').addClass('list');
+     $('.icon-windows').addClass('active');
+     $('.icon-list').removeClass('active')
+    });
+
+    $('.icon-list').on('click', function(){
+        $('.products__item').removeClass('list');
+        $('.icon-list').addClass('active');
+        $('.icon-windows').removeClass('active');
+        
+    });
+
 
     var mixer = mixitup('.products__inner-box');
 
